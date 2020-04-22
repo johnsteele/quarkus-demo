2fc3353a8b04aef734b6c299307988f99638ed84
John Steele <programjsteele@gmail.com>
2020-04-22 13:18:26 -0400

    setup

---
+++ src/main/java/com/example/demodiintegrationmetrics/DemoDiIntegrationMetricsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demodiintegrationmetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDiIntegrationMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDiIntegrationMetricsApplication.class, args);
+	}
+
+}
